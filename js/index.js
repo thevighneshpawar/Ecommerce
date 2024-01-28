9f31f454285f6b2e184ff22d05974904482a8e70
Vighnesh Pawar <vighneshpawar004@gmail.com>
2024-01-28 21:42:36 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+
+// header section
+
+const navOpen = document.querySelector('.mobile-open-btn');
+const navClose = document.querySelector('.mobile-close-btn');
+const primaryNavigation = document.getElementById('primary-navigation')
+
+navOpen.addEventListener('click',()=>{
+
+    const visibility = primaryNavigation.getAttribute('data-visible')
+
+    if(visibility === 'false'){
+        primaryNavigation.setAttribute('data-visible',true)
+        navClose.setAttribute('data-visible',true)
+    }else{
+        primaryNavigation.setAttribute('data-visible',false)
+        navClose.setAttribute('data-visible',false)
+    }
+
+})
+
+navClose.addEventListener('click',()=>{
+
+    const visibility = primaryNavigation.getAttribute('data-visible')
+
+    if (visibility === 'true') {
+        primaryNavigation.setAttribute('data-visible',false)
+        navClose.setAttribute('data-visible',false)
+    }
+})
+
+// =====cart menu ==========
+
+const shoppingBag = document.getElementById('cart-box')
+const CartItem = document.getElementById('cart-icon')
+const crossBtn = document.getElementById('cross-btn')
+
+shoppingBag.addEventListener('click',()=>{
+    const showCart = CartItem.getAttribute('data-visible')
+
+    if(showCart==='false'){
+        CartItem.setAttribute('data-visible',true)
+    }else{
+        CartItem.setAttribute('data-visible',false)
+    }
+})
+
+crossBtn.addEventListener('click',()=>{
+    const showCart = CartItem.getAttribute('data-visible')
+
+    if(showCart==='true'){
+        CartItem.setAttribute('data-visible',false)
+    }
+})
